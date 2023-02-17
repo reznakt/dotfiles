fd6f8f99c0e960cb112142cac51191a2937274dd
Tomáš Režňák <tomas.reznak@volny.cz>
2023-02-18 00:37:13 +0100

    Add push script

---
+++ dotfiles.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+
+FILEMAP=".filemap"
+
+
+function error() {
+    echo "error: $1" 1>&2
+    exit 1
+}
+
+
+if [ $# = 0 ] || [ $# -ge 3 ]; then
+    error "illegal number of arguments ($#)"
+fi
+
+
+if ! [ "$1" = "pull" ] && ! [ "$1" = "push" ]; then
+    error "illegal option '$1'"
+fi
+
+
+cat "$FILEMAP" | while read line; do
+    if [ -z "$line" ]; then
+        continue
+    fi
+
+    src=$(echo $line | cut -s -d " " -f 1)
+    dst=$(echo $line | cut -s -d " " -f 2)
+
+    if [ -z "$src" ] || [ -z "$dst" ]; then
+        error "unsupported format in $FILEMAP ($line)"
+    fi
+
+    src="${src/#\~/$HOME}"
+    dst="${dst/#\~/$HOME}"
+
+    case "$1" in
+        "pull")
+            if ! [ -f "$src" ]; then
+                continue
+            fi
+
+            cp -vb "$src" "$dst"
+            ;;
+        "push")
+            if ! [ -f "$dst" ]; then
+                continue
+            fi
+
+            cp -vb "$dst" "$src"
+    esac
+done
+
