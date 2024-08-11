510319f6fa9ee2456e9fbd008b0964e8347c0cb7
Tomáš Režňák <tomas.reznak@volny.cz>
2024-08-11 21:27:13 +0200

    Determine SSH remote URL automatically

--- .config/yadm/bootstrap.d/00-ssh-remote.sh
+++ .config/yadm/bootstrap.d/00-ssh-remote.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-REMOTE_URL="git@github.com:reznakt/dotfiles.git"
+REMOTE_URL=$(yadm remote get-url origin)
 
-echo "Switching yadm remote to SSH: $REMOTE_URL"
-yadm remote set-url origin $REMOTE_URL
+if echo "$REMOTE_URL" | grep --quiet "https://"; then
+  REPO_IDENTIFIER=$(echo "$REMOTE_URL" | sed -e "s#https://github.com/##" -e "s/\.git$//")
+  SSH_URL="git@github.com:$REPO_IDENTIFIER.git"
+
+  echo "Switching yadm remote to SSH: $SSH_URL"
+  yadm remote set-url origin "$SSH_URL"
+fi
 
