7c9bb493b326a14355e39a75af5f70b24fa5a2d3
Tomáš Režňák <tomas.reznak@volny.cz>
2024-08-12 02:38:11 +0200

    Allow specifying upt version; reinstall upt if versions don't match

--- .config/yadm/bootstrap.d/20-install-upt.sh
+++ .config/yadm/bootstrap.d/20-install-upt.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-UPT_TAG="v0.8.0"
-INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/sigoden/upt/$UPT_TAG/install.sh"
+UPT_VERSION="0.8.0"
+UPT_URL="https://raw.githubusercontent.com/sigoden/upt/v$UPT_VERSION/install.sh"
 INSTALL_DIR="$HOME/.local/bin"
 
-if ! command -v upt &> /dev/null; then
-  echo "Installing upt..."
-  curl -fsSL "$INSTALL_SCRIPT_URL" | bash -s -- --to "$INSTALL_DIR"
+function get_upt_version() {
+  "$INSTALL_DIR/upt" --help 2>&1 | sed -n "s/^Upt version: //p"
+}
+
+function install_upt () {
+  curl -fsSL "$UPT_URL" | bash -s -- --to "$INSTALL_DIR" --tag "v$UPT_VERSION" "$@"
+  chmod +x "$INSTALL_DIR/upt"
+}
+
+if ! [ -f "$INSTALL_DIR/upt" ]; then
+  echo "Installing upt ($UPT_VERSION)..."
+  install_upt
+elif ! [ $(get_upt_version) = "$UPT_VERSION" ]; then
+  echo "Upgrading upt ($(get_upt_version) -> $UPT_VERSION)..."
+  install_upt --force
 fi
 
