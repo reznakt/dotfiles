8d2a3cd517dccab19dc7a8c9970c2c02aa30de12
Tomáš Režňák <tomas.reznak@volny.cz>
2024-08-16 23:27:00 +0200

    Fix install packages hook

--- .config/yadm/bootstrap.d/30-install-packages.sh
+++ .config/yadm/bootstrap.d/30-install-packages.sh
@@ -4,28 +4,42 @@ PACKAGES="lsd topgrade"
 TO_INSTALL=""
 
 function has() {
-  command -v $1 &> /dev/null
+  command -v "$1" &> /dev/null
+}
+
+function add_package() {
+  package="$1"
+  command="$2"
+
+  if [ -z "$command" ]; then
+    command="$package"
+  fi
+
+  if ! has "$command"; then
+    TO_INSTALL="$TO_INSTALL $package"
+  fi
 }
 
 for package in $PACKAGES; do
-  if ! has $package; then
-    TO_INSTALL="$TO_INSTALL $package"
+  if ! has "$package"; then
+    add_package "$package"
   fi
 done
 
 # ripgrep is required for searching with telescope.nvim
-if has "nvim" && ! has "rg"; then
-  TO_INSTALL="$TO_INSTALL ripgrep"
+if has "nvim"; then
+  add_package "ripgrep" "rg"
 fi
 
 # minimal hyprland setup
 if has "Hyprland"; then
-  TO_INSTALL="$TO_INSTALL alacritty wofi"
+  add_package "wofi"
+  add_package "alacritty"
 fi
 
 if [ -n "$TO_INSTALL" ]; then
   echo "Installing missing packages..."
   sudo upt update
-  sudo upt install -y $TO_INSTALL
+  sudo upt install -y "$TO_INSTALL"
 fi
 
