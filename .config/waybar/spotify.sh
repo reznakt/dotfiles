9e69b07664a3f3c1ae240d3ca700288fb6c3430d
Tomáš Režňák <tomas.reznak@volny.cz>
2024-10-01 11:16:05 +0200

    De-duplicate spotify script

--- .config/waybar/spotify.sh
+++ .config/waybar/spotify.sh
@@ -4,11 +4,15 @@ spotify() {
   playerctl --player="spotify" "$@" 2> /dev/null
 }
 
+current_track() {
+  echo "$(spotify metadata artist) - $(spotify metadata title)"
+}
+
 player_status=$(spotify status)
 
 if [ "$player_status" = "Playing" ]; then
-  echo "$(spotify metadata artist) - $(spotify metadata title)"
+  current_track
 elif [ "$player_status" = "Paused" ]; then
-  echo " $(spotify metadata artist) - $(spotify metadata title)"
+  echo " $(current_track)"
 fi
 
