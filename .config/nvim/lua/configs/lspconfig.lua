6d4919ff068e79ed86bbd17c94dbe581e4b38191
Tomáš Režňák <tomas.reznak@volny.cz>
2024-10-28 23:29:59 +0100

    Add language servers

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,27 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
+
+local servers = {
+  "bashls",
+  "clangd",
+  "cmake",
+  "cssls",
+  "docker_compose_language_service",
+  "dockerls",
+  "eslint",
+  "gitlab_ci_ls",
+  "html",
+  "hyprls",
+  "marksman",
+  "nginx_language_server",
+  "pyright",
+  "tailwindcss",
+  "tsserver",
+  "vimls",
+  "yamlls"
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
