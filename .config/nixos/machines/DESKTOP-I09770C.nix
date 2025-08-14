0d91411170d4bce32b39c02fa29af9a823d23320
Tomáš Režňák <tomas.reznak@volny.cz>
2025-08-14 22:19:54 +0200

    Fix swapiness config

--- .config/nixos/machines/DESKTOP-I09770C.nix
+++ .config/nixos/machines/DESKTOP-I09770C.nix
@@ -9,7 +9,7 @@
   networking.hostName = "DESKTOP-I09770C";
   boot.initrd.luks.devices."luks-8ab404cd-daef-44f7-aeb7-608652583c50".device =
     "/dev/disk/by-uuid/8ab404cd-daef-44f7-aeb7-608652583c50";
-  kernel.sysctl = {
+  boot.kernel.sysctl = {
     "vm.swappiness" = 0;
   };
 }
