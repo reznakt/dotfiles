1caafb3db0aaa4bcefdced63da69700da9746272
Tomáš Režňák <tomas.reznak@volny.cz>
2025-08-14 04:07:20 +0200

    Fix laptop nixos config; add virtualbox

--- .config/nixos/home.nix
+++ .config/nixos/home.nix
@@ -7,51 +7,52 @@
     stateVersion = "25.05";
 
     packages = with pkgs; [
+      adwaita-icon-theme
+      bc
       bluemail
       brightnessctl
       btop-rocm
       caprine
+      desktop-file-utils
       fastfetch
       fd
+      fzf
       ghostty
+      gnome-themes-extra
+      go
       grim
       heroic
       hyprpicker
+      inotify-tools
       insomnia
+      jq
       labymod-launcher
+      libsForQt5.qt5ct
       lsd
       lunar-client
+      lurk
       nixfmt-rfc-style
+      nodejs # for neovim copilot plugin
       playerctl
+      pnpm
+      poetry
+      pre-commit
       pwvucontrol
+      qt6ct
       ripgrep
       ripgrep
       signal-desktop
       slurp
       spotify
       swaynotificationcenter
+      tmux
+      ungoogled-chromium
+      virtualenv
       vscode-fhs
       walker
       webcord
       wl-clipboard
       yadm
-      adwaita-icon-theme
-      bc
-      desktop-file-utils
-      fzf
-      gnome-themes-extra
-      go
-      inotify-tools
-      jq
-      libsForQt5.qt5ct
-      lurk
-      pnpm
-      poetry
-      pre-commit
-      qt6ct
-      tmux
-      ungoogled-chromium
-      virtualenv
       zsh-powerlevel10k
     ];
   };
