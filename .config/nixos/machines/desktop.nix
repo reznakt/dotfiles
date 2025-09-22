79cceb151001fb7e0d236c5a83e3c7a1217c8013
Tomáš Režňák <tomas.reznak@volny.cz>
2025-09-22 03:11:09 +0200

    Reformat machine configs

--- .config/nixos/machines/desktop.nix
+++ .config/nixos/machines/desktop.nix
@@ -7,10 +7,15 @@
 
 {
   networking.hostName = "desktop";
-  boot.initrd.luks.devices."luks-8ab404cd-daef-44f7-aeb7-608652583c50".device =
-    "/dev/disk/by-uuid/8ab404cd-daef-44f7-aeb7-608652583c50";
-  boot.kernel.sysctl = {
-    "vm.swappiness" = 0;
+
+  boot = {
+    initrd.luks.devices."luks-8ab404cd-daef-44f7-aeb7-608652583c50".device =
+      "/dev/disk/by-uuid/8ab404cd-daef-44f7-aeb7-608652583c50";
+
+    kernel.sysctl = {
+      "vm.swappiness" = 0;
+    };
   };
+
   security.sudo.wheelNeedsPassword = false;
 }
