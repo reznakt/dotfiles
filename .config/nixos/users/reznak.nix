7fb2f7e795199b209514f311d75d44aeffab07b7
Tomáš Režňák <tomas.reznak@volny.cz>
2025-11-24 01:44:20 +0100

    Remove wluma permission

--- .config/nixos/users/reznak.nix
+++ .config/nixos/users/reznak.nix
@@ -106,7 +106,6 @@ in
         "permission = ${lib.getExe pkgs.grim}, screencopy, allow"
         "permission = ${lib.escapeRegex (lib.getExe config.programs.hyprlock.package)}, screencopy, allow"
         "permission = ${pkgs.xdg-desktop-portal-hyprland}/libexec/.xdg-desktop-portal-hyprland-wrapped, screencopy, allow"
-        "permission = ${pkgs.wluma}/bin/.wluma-wrapped, screencopy, allow"
 
         "gesture = 3, horizontal, workspace"
       ]
