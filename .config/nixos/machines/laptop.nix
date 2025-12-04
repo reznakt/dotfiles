39972585c2e96c8b9357ab9ee58be24f5eff7258
Tomáš Režňák <tomas.reznak@volny.cz>
2025-12-04 14:52:38 +0100

    Remove redundant config

--- .config/nixos/machines/laptop.nix
+++ .config/nixos/machines/laptop.nix
@@ -25,11 +25,8 @@ in
   networking.hostName = "laptop";
 
   boot = {
-    initrd = {
-      luks.devices."luks-1953300c-4e53-4b52-a258-34d0b2b175bb".device =
-        "/dev/disk/by-uuid/1953300c-4e53-4b52-a258-34d0b2b175bb";
-      availableKernelModules = [ "amdgpu" ];
-    };
+    initrd.luks.devices."luks-1953300c-4e53-4b52-a258-34d0b2b175bb".device =
+      "/dev/disk/by-uuid/1953300c-4e53-4b52-a258-34d0b2b175bb";
 
     kernelModules = [ "typec_displayport" ];
 
